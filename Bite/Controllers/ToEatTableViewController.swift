0cf8467070b41aeb1cb70cf5316e4cc32c9dbe84
Phuong <phnguyen@knox.edu>
2018-02-28 14:29:18 -0600

    Added customized cells

--- Bite/Controllers/ToEatTableViewController.swift
+++ Bite/Controllers/ToEatTableViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import CoreData
 
-class ToEatTableViewController: UITableViewController {
-
+class ToEatTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate  {
+    
+    @IBOutlet weak var toEatTableView: UITableView!
+    
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+    var fetchedResultsController : NSFetchedResultsController<NSFetchRequestResult>? {
+        didSet {
+            // Whenever the frc changes, we execute the search and
+            // reload the table
+            fetchedResultsController?.delegate = self
+            executeSearch()
+            DispatchQueue.main.async {
+                self.toEatTableView.reloadData()
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        toEatTableView.delegate = self
+        let fr = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
+        print(try! delegate.stack.context.count(for: fr))
+        fr.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        //let pred = NSPredicate(format: "toEat == %@", argumentArray: [true])
+        //let pred = NSPredicate(format: "toEat == %@", NSNumber(value: true))
+        //fr.predicate = pred
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fr, managedObjectContext: delegate.stack.context , sectionNameKeyPath: nil, cacheName: nil)
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -20,32 +43,66 @@ class ToEatTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func executeSearch() {
+        if let fc = fetchedResultsController {
+            do {
+                try fc.performFetch()
+            } catch let e as NSError {
+                print("Error while trying to perform a search: \n\(e)\n\(String(describing: fetchedResultsController))")
+            }
+        }
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        if let fc = fetchedResultsController {
+            return (fc.sections?.count)!
+        } else {
+            return 0
+        }
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let fc = fetchedResultsController {
+            print("number of object \(fc.sections![section].numberOfObjects)")
+            return fc.sections![section].numberOfObjects
+        } else {
+            print("0")
+            return 0
+        }
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = fetchedResultsController?.object(at: indexPath) as! Item
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toEatCell", for: indexPath) as! ToEatCell
         // Configure the cell...
-
+        if item.image != nil  {
+            let p = UIImage(data: item.image! as Data)
+            cell.toEatImageView.image = p
+        } else { //else download new image
+            let ai = ActivityIndicator()
+            cell.toEatImageView.image = nil
+            ai.showLoader(cell.imageView!)
+            let _ = item.downloadImage(imagePath: item.image_url!, completionHandler: { (data, errorString) in
+                if errorString == nil {
+                    item.image = data! as NSData
+                    self.delegate.stack.save()
+                    DispatchQueue.main.async {
+                        ai.removeLoader()
+                        cell.toEatImageView.image = UIImage(data: data!)
+                    }
+                } else {
+                    ai.removeLoader()
+                }
+            })
+        }
+        cell.label.text = item.name
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
