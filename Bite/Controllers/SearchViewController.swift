63753ccc76bdaccf71d8635c5f940230594691f9
Phuong <phnguyen@knox.edu>
2018-03-08 22:41:41 -0600

    Updated UI colors

--- Bite/Controllers/SearchViewController.swift
+++ Bite/Controllers/SearchViewController.swift
@@ -144,12 +144,12 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedItem = items[(indexPath as NSIndexPath).row]
         let cell = tableView.cellForRow(at: indexPath) as! ItemViewCell
-        cell.cellView.backgroundColor = UIColor.gray
+        cell.cellView.backgroundColor = UIColor(named: "DeepLight")
         performSegue(withIdentifier: "detailSegue", sender: self)
     }
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         let cell = tableView.cellForRow(at: indexPath) as! ItemViewCell
-        cell.cellView.backgroundColor = UIColor(named: "Shade")
+        cell.cellView.backgroundColor = UIColor(named: "Light")
         
     }
     
