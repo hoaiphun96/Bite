a267f5528e0d4db024b3ae5717983f832ad6182e
Phuong <phnguyen@knox.edu>
2018-03-14 09:23:21 -0500

    Update: Core data classes

---
+++ Item+CoreDataClass.swift
@@ -0,0 +1,50 @@
+//
+//  Item+CoreDataClass.swift
+//  Bite
+//
+//  Created by Jamie Nguyen on 3/13/18.
+//  Copyright © 2018 Jamie Nguyen. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+    convenience init(itemName: String, brandName: String?, calories: Int?, image_url: String?, serving_quantity: String?, serving_unit: String?, context: NSManagedObjectContext) {
+        if let ent = NSEntityDescription.entity(forEntityName: "Item", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.name = itemName
+            self.brand_name = brandName
+            //self.calories = Int32(calories)
+            //self.calories = (Int32(Int(String(describing: calories))))!
+            if calories != nil {
+                let c = NSNumber(value: calories!)
+                self.calories = Int32(truncating: c)
+            }
+            self.image_url = image_url
+            self.serving_unit = serving_unit
+            self.serving_quantity = serving_quantity
+        } else {
+            fatalError("Unable to find Entity name")
+        }
+    }
+    
+    func downloadImage(imagePath:String, completionHandler: @escaping (_ imageData: Data?, _ errorString: String?) -> Void) -> URLSessionDataTask {
+        let session = URLSession.shared
+        let imgURL = NSURL(string: imagePath)
+        let request: NSURLRequest = NSURLRequest(url: imgURL! as URL)
+        
+        let task = session.dataTask(with: request as URLRequest) {data, response, downloadError in
+            if data == nil {
+                completionHandler(nil, "Could not download image \(imagePath)")
+            } else {
+                completionHandler(data, nil)
+            }
+        }
+        
+        task.resume()
+        return task
+    }
+}
---
+++ Item+CoreDataProperties.swift
@@ -0,0 +1,30 @@
+//
+//  Item+CoreDataProperties.swift
+//  Bite
+//
+//  Created by Jamie Nguyen on 3/13/18.
+//  Copyright © 2018 Jamie Nguyen. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Item {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
+        return NSFetchRequest<Item>(entityName: "Item")
+    }
+
+    @NSManaged public var brand_name: String?
+    @NSManaged public var calories: Int32
+    @NSManaged public var image: NSData?
+    @NSManaged public var image_url: String?
+    @NSManaged public var name: String?
+    @NSManaged public var serving_quantity: String?
+    @NSManaged public var serving_unit: String?
+    @NSManaged public var toAvoid: Bool
+    @NSManaged public var toEat: Bool
+
+}
