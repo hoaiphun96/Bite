c21873cd8e682265f079848664cd79d2f938593e
Phuong <phnguyen@knox.edu>
2018-03-15 00:29:03 -0500

    refactor: move Constants struct inside Client

--- Bite/Model/Constants.swift
+++ Bite/Model/Constants.swift
@@ -6,78 +6,79 @@
 //  Copyright © 2018 Jamie Nguyen. All rights reserved.
 //
 import UIKit
-struct Constants {
-    
-    // MARK: Nutritionix
-    struct Nutritionix {
-        static let APIBaseURL = "https://trackapi.nutritionix.com/v2/"
-    }
-    
-    // MARK: Nutritionix Parameter Keys
-    struct NutritionixParameterKeys {
-        static let ApplicationID = "method"
-        static let APIKey = "api_key"
-    }
-    
-    // MARK: Nutritionix Parameter Values
-    struct NutritionixParameterValues {
-        static let ApplicationID = "bc29c81f"
-        static let APIKey = "fadec995b490ade258351633d5a90ba9"
-        static let DisableJSONCallback = "1" /* 1 means "yes" */
-        static let SearchInstantMethod = "search/instant"
-   
-    }
-    
-    // MARK: Nutritionix Response Keys
-    struct NutritionixResponseKeys {
-      
-    }
-    
-    // MARK: Nutritionix Response Values
-    struct NutritionixResponseValues {
-        static let OKStatus = "ok"
-    }
-    
-    struct TempItem {
+extension Client {
+    struct Constants {
         
-        // MARK: Properties
-        
-        let name: String
-        let brand_name: String?
-        let calories: Int?
-        let image_url: String?
-        let serving_unit: String?
-        let serving_quantity: String?
-        var image: UIImage?
-        
-        // MARK: Initializers
-        
-        // construct a TempItem from a dictionary
-        init(dictionary: [String:AnyObject]) {
-            name = dictionary["food_name"] as! String
-            brand_name = dictionary["brand_name"] as? String
-            calories = dictionary["nf_calories"] as? Int
-            let pd = dictionary["photo"] as! [String: AnyObject]
-            image_url = pd["thumb"] as? String
-            serving_unit = dictionary["serving_unit"] as? String
-            serving_quantity = dictionary["serving_qty"] as? String
-            image = nil
+        // MARK: Nutritionix
+        struct Nutritionix {
+            static let APIBaseURL = "https://trackapi.nutritionix.com/v2/"
         }
         
-      
+        // MARK: Nutritionix Parameter Keys
+        struct NutritionixParameterKeys {
+            static let ApplicationID = "method"
+            static let APIKey = "api_key"
+        }
         
-        static func itemsFromResults(_ results: [[String:AnyObject]]) -> [TempItem] {
-            var items = [TempItem]()
+        // MARK: Nutritionix Parameter Values
+        struct NutritionixParameterValues {
+            static let ApplicationID = "bc29c81f"
+            static let APIKey = "fadec995b490ade258351633d5a90ba9"
+            static let DisableJSONCallback = "1" /* 1 means "yes" */
+            static let SearchInstantMethod = "search/instant"
+       
+        }
+        
+        // MARK: Nutritionix Response Keys
+        struct NutritionixResponseKeys {
+          
+        }
+        
+        // MARK: Nutritionix Response Values
+        struct NutritionixResponseValues {
+            static let OKStatus = "ok"
+        }
+        
+        struct TempItem {
             
-            // iterate through array of dictionaries, each Movie is a dictionary
-            for result in results {
-                items.append(TempItem(dictionary: result))
+            // MARK: Properties
+            
+            let name: String
+            let brand_name: String?
+            let calories: Int?
+            let image_url: String?
+            let serving_unit: String?
+            let serving_quantity: String?
+            var image: UIImage?
+            
+            // MARK: Initializers
+            
+            // construct a TempItem from a dictionary
+            init(dictionary: [String:AnyObject]) {
+                name = dictionary["food_name"] as! String
+                brand_name = dictionary["brand_name"] as? String
+                calories = dictionary["nf_calories"] as? Int
+                let pd = dictionary["photo"] as! [String: AnyObject]
+                image_url = pd["thumb"] as? String
+                serving_unit = dictionary["serving_unit"] as? String
+                serving_quantity = dictionary["serving_qty"] as? String
+                image = nil
             }
             
-            return items
+          
+            
+            static func itemsFromResults(_ results: [[String:AnyObject]]) -> [TempItem] {
+                var items = [TempItem]()
+                
+                // iterate through array of dictionaries, each Movie is a dictionary
+                for result in results {
+                    items.append(TempItem(dictionary: result))
+                }
+                
+                return items
+            }
         }
+
     }
-
 }
-
 
